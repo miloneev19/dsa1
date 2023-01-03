936e51ab018d41033cacb778a574d952aea29bfe
Milonee Vyas <115082114+miloneev19@users.noreply.github.com>
2023-01-03 09:16:22 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,113 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int front=-1,rear=-1,n=5 ;
-int cqueue[5];
-
-void enqueue(int x)
+void heapify(int arr[],int n,int i)
 {
-    if((front==0 && rear==n-1) || front==rear+1)
-    {
-     cout<<"Queue Overflow"<<endl;
-     return;
-    }
-    if(front==-1)
-    {
-        front=0;
-        rear=0;
-    }
-    else
-    {
-        if(rear==n-1)
-        {
-            rear=0;
-        }
-        else
-        {
-            rear=rear+1;
-        }
-    }
-    cqueue[rear]=x;
-}
+    int largest = i;
+    int l=2*i+1;
+    int r=2*i+2;
 
-void dequeue()
+    if(l<n && arr[l]>arr[largest])
 {
-    if(front==-1)
+        largest=l;
+}
+    if(r<n && arr[r]>arr[largest])
     {
-        cout<<"Queue is empty, element can't be popped "<<endl;
-        return;
+        largest=r;
     }
-    cout<<"popped out element is "<<cqueue[front]<<endl;
-    if(front==rear)
+
+    if(largest!=i)
     {
-        front=-1;
-        rear=-1;
-    }
-    else
-    {
-        if(front==n-1)
-        {
-            front=0;
-        }
-        else
-        {
-            front=front+1;
-        }
+        swap(arr[largest],arr[i]);
+        heapify(arr,n,largest);
     }
 }
 
-
-void display() {
-    if (front == -1) {
-        cout << "queue is empty" << endl;
+void heapsort(int arr[],int n)
+{
+    for(int i=(n/2)-1;i>=0;i--)
+    {
+        heapify(arr,n,i);
     }
-    cout << "queue elements are" << endl;
-    if (front <= rear) {
-        while (front <= rear) {
-            cout << cqueue[front] << " ";
-            front++;
-        }
-    } else {
-        while (front <= n - 1) {
-            cout << cqueue[front] << " ";
-            front++;
-        }
-        front = 0;
-        while (front <= rear) {
-            cout << cqueue[front] << " ";
-            front++;
-        }
+    //to build array
+    for(int i=n-1;i>=0;i--)
+    {
+        swap(arr[i],arr[0]);
+        heapify(arr,i,0);
     }
-    cout<<endl;
-
 }
 
+void display(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
 int main() {
-    int ch, val;
-    cout<<"1)Insert\n";
-    cout<<"2)Delete\n";
-    cout<<"3)Display forward\n";
-    cout<<"4)Exit\n";
-    do {
-        cout<<"Enter choice : "<<endl;
-        cin>>ch;
-        switch(ch) {
-            case 1:
-                cout<<"Input for insertion: "<<endl;
-                cin>>val;
-                enqueue(val);
-                break;
-            case 2:
-                dequeue();
-                break;
-            case 3:
-                display();
-                break;
-            case 4:
-                cout<<"Exit\n";
-                break;
-            default: cout<<"Incorrect!\n";
-        }
-    } while(ch != 4);
+    int arr[]={19,31,2,78,91,9};
+    int n=5;
+    heapsort(arr,n);
+    display(arr,n);
     return 0;
 }
